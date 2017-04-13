88a4c8d7805d79644551a8a6fb38e341b4564f60
Owlypixel <owlypixel@gmail.com>
2017-04-13 23:00:17 +0300

    Using slots in components

---
+++ quote-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='container'>
+    <div class="row">
+      <div class="col-xs-12">
+        <my-quote>
+          <h1 slot="title">{{ quoteTitle }}</h1>
+          <p slot="content">What an awesome quote</p>
+        </my-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue';
+
+  export default {
+    data: function(){
+      return {
+        quoteTitle: 'Quote Title'
+      }
+    },
+    components: {
+      'my-quote': Quote
+    }
+  }
+</script>
+
+<style>
+
+</style>
