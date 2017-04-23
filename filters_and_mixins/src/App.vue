f24c43812586c029297f369c827e22557998e11f
Owlypixel <owlypixel@gmail.com>
2017-04-23 23:39:31 +0300

    Mixins

--- filters_and_mixins/src/App.vue
+++ filters_and_mixins/src/App.vue
@@ -9,18 +9,21 @@
         <ul>
           <li v-for="fruit in filteredFruits">{{ fruit }}</li>
         </ul>
+        <hr>
+          <app-list></app-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import List from './List.vue';
+import { fruitMixin } from './fruitMixin.js';
+
 export default {
   data(){
     return {
       text: 'Hello there!',
-      fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
-      filterText: '' 
     }
   },
   filters: {
@@ -28,13 +31,10 @@ export default {
       return value.toUpperCase();
     }
   },
-  computed: {
-    filteredFruits(){
-      return this.fruits.filter((el) => {
-          return el.match(this.filterText);
-      });
-    }
-  }
+  components: {
+    appList: List
+  },
+  mixins: [fruitMixin]
  
 }
 </script>
---
+++ filters_and_mixins/src/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Filters and mixins</h1>
+    <input v-model='filterText'>
+    <ul>
+      <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { fruitMixin } from './fruitMixin.js';
+
+export default {
+ mixins: [fruitMixin] 
+}
+</script>
+
+<style>
+
+</style>
