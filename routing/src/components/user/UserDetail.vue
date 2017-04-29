8029f73cc43a834f339ad24b4aff77875bd09653
Owlypixel <owlypixel@gmail.com>
2017-04-29 23:23:00 +0300

    Vue Router, dynamic links, child routes

---
+++ routing/src/components/user/UserDetail.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+    	<h3>Some User Details</h3>
+    	<p>User loaded has ID: {{ $route.params.id }}</p>
+    	<router-link class="btn btn-primary" tag="button" :to="'/user/' + $route.params.id + '/edit'">Edit User</router-link>
+    </div>
+</template>
---
+++ routing/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+import {routes} from './routes';
+
+const router = new VueRouter({ routes, mode: 'history' });
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
